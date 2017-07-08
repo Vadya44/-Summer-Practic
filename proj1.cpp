eb0473978a90336eb972b030ff7c70807f0a2a39
Vadya44 <44vadimko@gmail.com>
2017-07-08 12:20:20 +0300

    О формировании оценок

--- proj1.cpp
+++ proj1.cpp
@@ -3,16 +3,5 @@
 using namespace std;
 int n, k;
 int main(int argc, char* argv[]) {
-    cin >> n >> k;
-    int a[n];
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    string res = "";
-    for (int j = 0; j < k; ++j) {
-        int buff;
-        cin >> buff;
-        cout << BinarySearch(0, n - 1, buff, a) << "\n";
-    }
-    cout << res;
+
 }
