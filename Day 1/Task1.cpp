c0bcd27204bd453e8b8d13c0bc2920e3c495a04f
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-08 16:39:49 +0300

    Add newlines in Task1.cpp

--- Day 1/Task1.cpp
+++ Day 1/Task1.cpp
@@ -10,29 +10,40 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     int N, k;
+
     cout << "Input N:" << "\n";
     cin >> N;
+
     cout << "Input k:" << "\n";
     cin >> k;
+
     int arr[N];
+
     cout << "Array is:" << "\n";
+
     for(int i = 0; i < N; i++) {
         arr[i] = i + 1;
         cout << arr[i] << "; ";
     }
+
     // Shifting array left for k positions
     for(int i = 0; i < k; i++) {
         {
             int temp = arr[0];
+
             for(int j = 0; j < N - 1; j++) {
                 arr[j] = arr[j + 1];
             }
+
             arr[N - 1] = temp;
         }
     }
+
     cout << "\n" << "Array after shifting is:" << "\n";
+
     for(int i = 0; i < N; i++) {
         cout << arr[i] << "; ";
     }
+
     return 0;
 }
