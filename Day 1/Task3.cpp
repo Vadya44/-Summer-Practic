a3b33157a4f428347f64c37e12417b708d31d076
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-08 16:32:40 +0300

    Reformat code in Task3.cpp

--- Day 1/Task3.cpp
+++ Day 1/Task3.cpp
@@ -2,35 +2,34 @@
  * Написать бабл сорт для массива длины N(вводится с клавиатуры).
  */
 #include <iostream>
+
 using namespace std;
-int main(int argc, char* argv[]) {
+
+int main(int argc, char* argv[])
+{
     int N;
     cout << "Input N:" << "\n";
     cin >> N;
     int arr[N];
     cout << "Array is:" << "\n";
-    for (int i = 0; i < N; i++)
-    {
+    for(int i = 0; i < N; i++) {
         arr[i] = rand() % 10;
         cout << arr[i] << "; ";
     }
     // Bubble sort O(n^2)
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N - i; j++)
-        {
-            if (arr[j] > arr[j+1])
-            {
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N - i; j++) {
+            if(arr[j] > arr[j + 1]) {
                 int buff = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = buff;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = buff;
             }
         }
 
     }
     cout << "\n" << "Array after compression is:" << "\n";
-    for (int i = 0; i < N; i++)
-    {
+    for(int i = 0; i < N; i++) {
         cout << arr[i] << "; ";
     }
-    return  0;
+    return 0;
 }
