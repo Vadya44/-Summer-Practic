5fc7c2bde370c1e3c9cbd7e9882415d04c1d41cd
Vadya44 <44vadimko@gmail.com>
2017-07-09 01:07:32 +0300

    fixes

--- Day 1/Task2.cpp
+++ Day 1/Task2.cpp
@@ -1,49 +1,62 @@
 /*
- * Создать массив длины N(вводится с клавиатуры).
- * Переместить все нули в конце массива.
+ * Task 1
+ * Создать массив длинной N(вводится с клавиатуры).
+ * Выполнить сдвиг влево на k позиций(вводится с клавиатуры).
  */
 #include <iostream>
+
 using namespace std;
 
-const int INFINITI = 1e9;
-int arr[INFINITI];
+/**
+ * Reverse the order of the elements at indices [begin; end] in the array
+ * @param arr The array whose elements are to be reversed
+ * @param begin Start index (inclusive) of the interval to be reversed
+ * @param end Start index (inclusive) of the interval to be reversed
+ */
+void reserse_array(int* arr, int begin, int end)
+{
+    for(int i = 0; i < (end - begin + 1) / 2; i++) {
+        int temp = arr[begin + i];
+        arr[begin + i] = arr[end - i];
+        arr[end - i] = temp;
+    }
+}
 
 int main(int argc, char* argv[])
 {
-    int N;
+    int N, k;
 
-    cout << "Enter N:" << endl;
+    cout << "Enter N: ";
     cin >> N;
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    int* arr = new int[N];
 
     cout << "Array is:" << endl;
 
     for(int i = 0; i < N; i++) {
-        arr[i] = rand() % 10;
+        arr[i] = i + 1;
         cout << arr[i] << "; ";
     }
 
-    int position = N - 1;
+    k %= N;
 
-    // Перемещаем в конец массива все нули
-    for(int i = 0; i < position; i++) {
-        if(arr[i] == 0) {
-            if(arr[position] != 0) {  //Случай, когда элемент в конце не 0
-                arr[i] = arr[position];
-                arr[position--] = 0;
-            }
-            else if(arr[position] == 0) {  // Случай, когда элемент в конце равен нулю, поэтому перемещаемся
-                while(arr[--position] == 0) { }  // к близжайшему от конца элементу, который не равен нулю.
-                arr[i] = arr[position];
-                arr[position--] = 0;
-            }
-        }
-    }
+    /*
+     * Shift array to the left by k positions in O(n) time
+     */
+    reserse_array(arr, 0, k - 1);
+    reserse_array(arr, k, N - 1);
+    reserse_array(arr, 0, N - 1);
 
-    cout << endl << "Array after compression is:" << endl;
+    cout << endl << "Array after shifting is:" << endl;
 
     for(int i = 0; i < N; i++) {
         cout << arr[i] << "; ";
     }
+
+    delete[] arr;
 
     return 0;
 }
